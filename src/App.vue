6e8c56e043f01f12aa613320e68b27a2c8895d35
T660046@tcrbank.com <T660046@tcrbank.com>
2024-01-19 14:35:54 +0700

    add v-overlay

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+  const globalStore = useGlobalStore()
+</script>
+
 <template>
   <router-view />
+  <v-overlay
+    v-model="globalStore.loading"
+    class="align-center justify-center"
+    persistent>
+    <v-card>
+      <v-card-text class="text-center">
+        <v-progress-circular
+          size="32"
+          color="green"
+          indeterminate></v-progress-circular>
+        <p class="mt-2">Loading...</p>
+      </v-card-text>
+    </v-card>
+  </v-overlay>
 </template>
