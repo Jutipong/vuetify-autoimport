142967458f833cb667e86587400601cef82c4719
Jutipong <techcom.ec20@gmail.com>
2024-05-16 00:33:02 +0700

    refactor: Update customer data handling in ActionModal-01.vue

--- src/components/customer/action-modal-01.vue
+++ src/components/customer/action-modal-01.vue
@@ -7,64 +7,64 @@ const props = defineProps<CustomerType>()
 const emit = defineEmits<{ update: [value: CustomerType] }>()
 const modalOpen = defineModel<boolean>()
 
-const customer = ref<CustomerType>({})
+const customer = ref<CustomerType>({} as CustomerType)
 
 onMounted(() => {
 // ใน props ค่าของ defindmodel จะถูกเก็บไว้ใน props ด้วย ดังนั้น ไม่ว่าจะค่า props หรือ defindmodel ก็สามารถใช้ watch ได้
-  watch(props, (newVal) => {
-    if (!modalOpen.value) {
-      emit('update', customer.value)
-      customer.value = {}
-      return
-    }
+    watch(modalOpen, (newVal) => {
+        if (!newVal) {
+            emit('update', customer.value)
+            customer.value = {} as CustomerType
+            return
+        }
 
-    Object.assign(customer, newVal)
-  })
+        Object.assign(customer, props)
+    })
 })
 
 function closeModal() {
-  modalOpen.value = false
+    modalOpen.value = false
 }
 </script>
 
 <template>
-  <Teleport to="body">
-    <VRow justify="center">
-      <VDialog v-model="modalOpen" persistent width="1024">
-        <VCard>
-          <VCardTitle>
-            <VChip color="success" prepend-icon="product.id ?">
-              Modal 02: {{ customer }}
-            </VChip>
-          </VCardTitle>
-          <VDivider />
-          <VCardText>
-            <VRow>
-              <VCol cols="12" md="4">
-                <VTextField v-model="customer.Name" label="Name" />
-              </VCol>
-              <VCol cols="12" md="4">
-                <VTextField v-model="customer.Last" label="Last" />
-              </VCol>
-              <VCol cols="12" md="4" />
-            </VRow>
-            <VRow>
-              <VCol cols="12" md="4">
-                <!-- <VTextField v-model="product.stock" label="Stock" /> -->
-              </VCol>
-              <VCol cols="12" md="4">
-                <!-- <VTextField v-model="product.brand" label="Brand" /> -->
-              </VCol>
-              <VCol cols="12" md="4" />
-            </VRow>
-          </VCardText>
-          <VDivider />
-          <VCardActions>
-            <VSpacer />
-            <VBtn color="warning" prepend-icon="mdi-close" text="Close" @click="closeModal" />
-          </VCardActions>
-        </VCard>
-      </VDialog>
-    </VRow>
-  </Teleport>
+    <Teleport to="body">
+        <VRow justify="center">
+            <VDialog v-model="modalOpen" persistent width="1024">
+                <VCard>
+                    <VCardTitle>
+                        <VChip color="success" prepend-icon="product.id ?">
+                            Modal 02: {{ customer }}
+                        </VChip>
+                    </VCardTitle>
+                    <VDivider />
+                    <VCardText>
+                        <VRow>
+                            <VCol cols="12" md="4">
+                                <VTextField v-model="customer.Name" label="Name" />
+                            </VCol>
+                            <VCol cols="12" md="4">
+                                <VTextField v-model="customer.Last" label="Last" />
+                            </VCol>
+                            <VCol cols="12" md="4" />
+                        </VRow>
+                        <VRow>
+                            <VCol cols="12" md="4">
+                                <!-- <VTextField v-model="product.stock" label="Stock" /> -->
+                            </VCol>
+                            <VCol cols="12" md="4">
+                                <!-- <VTextField v-model="product.brand" label="Brand" /> -->
+                            </VCol>
+                            <VCol cols="12" md="4" />
+                        </VRow>
+                    </VCardText>
+                    <VDivider />
+                    <VCardActions>
+                        <VSpacer />
+                        <VBtn color="warning" prepend-icon="mdi-close" text="Close" @click="closeModal" />
+                    </VCardActions>
+                </VCard>
+            </VDialog>
+        </VRow>
+    </Teleport>
 </template>
