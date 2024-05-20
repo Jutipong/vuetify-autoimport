a2fb794564ee083c9f3abdd81e4a46ffd69b5ce0
jutipong subin <T660046@tcrbank.com>
2024-05-20 16:24:27 +0700

    refactor: Update eslint.config.mjs to disable console and debugger statements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,6 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu(
     {
-        // Enable stylistic formatting rules
-        // Or customize the stylistic rules
         stylistic: {
             indent: 4,
             overrides: {
@@ -12,32 +10,23 @@ export default antfu(
                 // 'no-restricted-syntax': 'off',
             },
         },
-
-        // TypeScript and Vue are auto-detected, you can also explicitly enable them:
         typescript: true,
         vue: true,
-
         rules: {
-            'vue/valid-v-slot': ['error', { allowModifiers: true }],
+            'vue/valid-v-slot': ['error', {
+                allowModifiers: true,
+            }],
             'vue/no-mutating-props': ['error', {
                 shallowOnly: true,
             }],
         },
-
         // Disable jsonc and yaml support
         jsonc: true,
         yaml: true,
-
         // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
         ignores: [
             '**/fixtures',
             'vite-env.d.ts',
-            // ...globs,
         ],
-        formatters: {
-            css: true,
-            html: true,
-            markdown: 'prettier',
-        },
     },
 )
