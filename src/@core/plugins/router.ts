08bd4aba087423e1e949504fc8bcd3ff9398c314
jutipong subin <T660046@tcrbank.com>
2024-06-10 16:42:20 +0700

    refactor: Update VueDevTools configuration and npm dependencies

--- src/@core/plugins/router.ts
+++ src/@core/plugins/router.ts
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    extendRoutes: (routes: any) => setupLayouts(routes),
+    routes: setupLayouts(routes),
     scrollBehavior() { return { top: 0 } },
 })
 
