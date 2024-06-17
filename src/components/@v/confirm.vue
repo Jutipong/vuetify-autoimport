9ee0630a956198c07e534c4b8b068fd110f0b4d5
Jutipong <techcom.ec20@gmail.com>
2024-06-18 00:42:36 +0700

    refactor: Update confirm component to use Vuetify 3.x syntax
    - Replace deprecated Vuetify 2.x components with equivalent Vuetify 3.x components in confirm.vue

--- src/components/@v/confirm.vue
+++ src/components/@v/confirm.vue
@@ -3,47 +3,42 @@ import store from '@/utils/confirm'
 </script>
 
 <template>
-    <teleport to="body">
-        <div v-if="store.state.isOpen">
-            <v-row justify="center">
-                <v-dialog v-model="store.state.isOpen" persistent width="auto">
-                    <v-card
-                        min-width="400px"
-                        min-height="160px"
-                    >
-                        <v-card-title class="pa-1 pl-2">
-                            <v-icon
-                                :color="store.state.options.btnOk?.color ?? ''"
-                                :icon="store.state.options.iconTitle ?? ' mdi-alert-circle-outline'"
-                            />
-                            {{ store.state.title }}
-                        </v-card-title>
-                        <v-divider />
-                        <v-card-text>
-                            <span v-html="store.state.message" />
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-spacer />
-                            <v-btn
-                                v-if="!store.state.options.btnCancelDisabled"
-                                min-width="116px"
-                                :color="store.state.options.btnCancel?.color ?? 'default'"
-                                :prepend-icon="store.state.options.btnCancel?.icon ?? 'mdi-close'"
-                                :text="store.state.options.btnCancel?.text ?? 'Cancel'"
-                                @click="store.onCancel()"
-                            />
-                            <v-btn
-                                v-if="!store.state.options.btnOkDisabled"
-                                min-width="116px"
-                                :color="store.state.options.btnOk?.color ?? 'success'"
-                                :prepend-icon="store.state.options.btnOk?.icon ?? 'mdi-check'"
-                                :text="store.state.options.btnOk?.text ?? 'Yes'"
-                                @click="store.onOk()"
-                            />
-                        </v-card-actions>
-                    </v-card>
-                </v-dialog>
-            </v-row>
-        </div>
-    </teleport>
+    <Teleport to="body">
+        <VRow justify="center">
+            <VDialog v-model="store.state.isOpen" persistent width="auto">
+                <VCard min-height="160px">
+                    <VCardTitle class="pa-1 pl-2">
+                        <VIcon
+                            :color="store.state.options.btnOk?.color ?? ''"
+                            :icon="store.state.options.iconTitle ?? ' mdi-alert-circle-outline'"
+                        />
+                        {{ store.state.title }}
+                    </VCardTitle>
+                    <VDivider />
+                    <VCardText>
+                        <span v-html="store.state.message" />
+                    </VCardText>
+                    <VCardActions>
+                        <VSpacer />
+                        <VBtn
+                            v-if="!store.state.options.btnCancelDisabled"
+                            min-width="116px"
+                            :color="store.state.options.btnCancel?.color ?? 'default'"
+                            :prepend-icon="store.state.options.btnCancel?.icon ?? 'mdi-close'"
+                            :text="store.state.options.btnCancel?.text ?? 'Cancel'"
+                            @click="store.onCancel()"
+                        />
+                        <VBtn
+                            v-if="!store.state.options.btnOkDisabled"
+                            min-width="116px"
+                            :color="store.state.options.btnOk?.color ?? 'success'"
+                            :prepend-icon="store.state.options.btnOk?.icon ?? 'mdi-check'"
+                            :text="store.state.options.btnOk?.text ?? 'Yes'"
+                            @click="store.onOk()"
+                        />
+                    </VCardActions>
+                </VCard>
+            </VDialog>
+        </VRow>
+    </Teleport>
 </template>
