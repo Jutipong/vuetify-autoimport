0f185f3909be5df2138b08aae735eaa6bfdb4bd0
Jutipong <techcom.ec20@gmail.com>
2024-07-03 16:50:52 +0700

    refactor: Update cache timeout handling in api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -10,13 +10,39 @@ const axiosInstance = axios.create({
 })
 
 setupCache(axiosInstance, {
-    ttl: 5 * 60 * 1000,
     storage: buildWebStorage(sessionStorage, 'axios-cache:'),
     generateKey: req => generateCacheKey(req),
+    update: config => updateCache(config),
 })
 
+function updateCache(config: any) {
+    if (config.config.cacheTimeout) {
+        config.config.cache.ttl = getCacheTimeout(config.config.cacheTimeout)
+    }
+}
+
+function getCacheTimeout(timeout: CacheTimeout) {
+    switch (timeout) {
+        case '30sec':
+            return 30 * 1000
+        case '1min':
+            return 60 * 1000
+        case '5min':
+            return 5 * 60 * 1000
+        case '10min':
+            return 10 * 60 * 1000
+        case '15min':
+            return 15 * 60 * 1000
+        case '30min':
+            return 30 * 60 * 1000
+        case '1hour':
+            return 60 * 60 * 1000
+        default:
+            return 0
+    }
+}
+
 function generateCacheKey(config: any) {
-    // Basic example: URL + Method
     let key = `${config.url}_${config.method}`
 
     if (config.params) {
@@ -24,7 +50,6 @@ function generateCacheKey(config: any) {
         key += `?${params}`
     }
 
-    // For POST requests, you might want to include the request body
     if (config.data && config.method.toLowerCase() === 'post') {
         key += `_${JSON.stringify(config.data)}`
     }
@@ -92,7 +117,13 @@ axiosInstance.interceptors.response.use(({ config, data }: any): AxiosResponse<a
     return { data } as AxiosResponse<any, any>
 }, handleError)
 
-type ApiOptions = { isLoading?: boolean, useCache?: boolean } & AxiosRequestConfig
+type CacheTimeout = '30sec' | '1min' | '5min' | '10min' | '15min' | '30min' | '1hour'
+
+type ApiOptions = {
+    isLoading?: boolean
+    useCache?: boolean
+    cacheTimeout?: CacheTimeout
+} & AxiosRequestConfig
 
 export default {
     get: async <TResponse>(url: string, config?: ApiOptions) => {
