db7920d9c4bff284b64b9136e1dbc8e511cee88b
Jutipong <techcom.ec20@gmail.com>
2024-07-05 21:39:58 +0700

    refactor: Update form validation and error handling in validate/form.vue

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,46 +1,102 @@
 import { ref } from 'vue'
 import type { z } from 'zod'
 
+//
+// version 2
+//
 function useValidate<T extends z.ZodType<any, any>>(schema: T) {
-    const data = ref({} as z.infer<T>)
-    const errors = ref({} as Record<string, string>)
-    const hasValidated = ref(false)
+type FormData = z.infer<typeof schema>
+type ErrorField = Partial<keyof FormData>
 
-    function validate(): boolean {
-        hasValidated.value = true
+const formData = ref({} as FormData)
+const errors = ref<Record<ErrorField, string>>()
+const hasValidated = ref(false)
 
-        const result = schema.safeParse(data.value)
+function validate(): boolean {
+    hasValidated.value = true
 
-        if (result.success) {
-            errors.value = {}
-            return true
-        }
+    const result = schema.safeParse(formData.value)
 
-        const formattedErrors = {} as Record<string, string>
-
-        result.error.errors.forEach((error) => {
-            const fieldName = error.path[0] as string
-            formattedErrors[fieldName] = error.message
-        })
-
-        errors.value = formattedErrors
-        return false
+    if (result.success) {
+        errors.value = {} as Record<ErrorField, string>
+        return true
     }
 
-    function reset() {
-        errors.value = {} as Record<string, string>
-        data.value = {} as z.infer<T>
-        hasValidated.value = false
-    }
+    const formattedErrors = {} as Record<ErrorField, string>
 
-    watch(data, () => {
-        if (!hasValidated.value)
-            return
+    result.error.errors.forEach((error) => {
+        const fieldName = error.path[0] as ErrorField
+        formattedErrors[fieldName] = error.message
+    })
 
-        validate()
-    }, { deep: true })
+    errors.value = formattedErrors
+    return false
+}
 
-    return { data, errors, validate, reset }
+function reset() {
+    errors.value = {} as Record<ErrorField, string>
+    formData.value = {} as z.infer<T>
+    hasValidated.value = false
+}
+
+watch(formData, () => {
+    if (!hasValidated.value)
+        return
+
+    validate()
+}, { deep: true })
+
+return { data: formData, errors, validate, reset }
 }
 
 export { useValidate }
+
+//
+// version 1
+//
+// import { ref } from 'vue'
+// import type { z } from 'zod'
+
+// function useValidate<T extends z.ZodType<any, any>>(schema: T) {
+//     const data = ref({} as z.infer<T>)
+//     const errors = ref({} as Record<string, string>)
+//     const hasValidated = ref(false)
+
+//     function validate(): boolean {
+//         hasValidated.value = true
+
+//         const result = schema.safeParse(data.value)
+
+//         if (result.success) {
+//             errors.value = {}
+//             return true
+//         }
+
+//         const formattedErrors = {} as Record<string, string>
+
+//         result.error.errors.forEach((error) => {
+//             const fieldName = error.path[0] as string
+//             formattedErrors[fieldName] = error.message
+//         })
+
+//         errors.value = formattedErrors
+//         return false
+//     }
+
+//     function reset() {
+//         errors.value = {} as Record<string, string>
+//         data.value = {} as z.infer<T>
+//         hasValidated.value = false
+//     }
+
+//     watch(data, () => {
+//         if (!hasValidated.value)
+//             return
+
+//         validate()
+//     }, { deep: true })
+
+//     return { data, errors, validate, reset }
+// }
+
+// export { useValidate }
