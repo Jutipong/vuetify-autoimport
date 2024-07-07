17bd1d92457603518c46185b17c4b38def789679
Jutipong <techcom.ec20@gmail.com>
2024-07-07 14:31:15 +0700

    refactor: Update useDataTable to use generic type for result in DataTableType

--- src/utils/data-table.ts
+++ src/utils/data-table.ts
@@ -1,9 +1,9 @@
-function useDataTable<TData>(headers: DataTableHeaderType[]) {
+function useDataTable<TDataTableResultType>(headers: DataTableHeaderType[]) {
     const table = reactive({
         headers: headers as DataTableHeaderType[],
         options: { ...dataTableOptionConst },
         result: { ...dataTableResultType },
-    } as DataTableType<TData>)
+    } as DataTableType<TDataTableResultType>)
 
     return table
 }
