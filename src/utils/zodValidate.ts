c47cc02273d0bdfe464e15fa0f4193a62239f47f
Jutipong <techcom.ec20@gmail.com>
2024-07-17 00:59:32 +0700

    refactor: Remove unused code and comments from zodValidate.ts

--- src/utils/zodValidate.ts
+++ src/utils/zodValidate.ts
@@ -1,8 +1,5 @@
 import { z } from 'zod'
 
-//
-// version 2
-//
 function useZodValidate<T extends z.ZodType<any, any>>(schema: T) {
 type FormData = z.infer<typeof schema>
 type ErrorField = Partial<keyof FormData>
@@ -56,53 +53,3 @@ function requiredNumber(message: string, zodType: z.ZodType<any, any>[] = []) {
 }
 
 export { useZodValidate, requiredString, requiredNumber }
-
-//
-// version 1
-//
-// import { ref } from 'vue'
-// import type { z } from 'zod'
-
-// function useValidate<T extends z.ZodType<any, any>>(schema: T) {
-//     const data = ref({} as z.infer<T>)
-//     const errors = ref({} as Record<string, string>)
-//     const hasValidated = ref(false)
-
-//     function validate(): boolean {
-//         hasValidated.value = true
-
-//         const result = schema.safeParse(data.value)
-
-//         if (result.success) {
-//             errors.value = {}
-//             return true
-//         }
-
-//         const formattedErrors = {} as Record<string, string>
-
-//         result.error.errors.forEach((error) => {
-//             const fieldName = error.path[0] as string
-//             formattedErrors[fieldName] = error.message
-//         })
-
-//         errors.value = formattedErrors
-//         return false
-//     }
-
-//     function reset() {
-//         errors.value = {} as Record<string, string>
-//         data.value = {} as z.infer<T>
-//         hasValidated.value = false
-//     }
-
-//     watch(data, () => {
-//         if (!hasValidated.value)
-//             return
-
-//         validate()
-//     }, { deep: true })
-
-//     return { data, errors, validate, reset }
-// }
-
-// export { useValidate }
