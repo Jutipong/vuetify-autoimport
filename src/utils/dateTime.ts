c9c26a4d21aa4a5892ab4fc7c4793084a7cab382
Jutipong <techcom.ec20@gmail.com>
2024-07-17 00:34:59 +0700

    refactor: Update timeConfig function to include additional time intervals

--- src/utils/dateTime.ts
+++ src/utils/dateTime.ts
@@ -6,8 +6,8 @@ export type DateFormat =
     'HH:mm' | 'HH:mm:ss' | 'HH:mm A' | 'HH:mm:ss A'
 
 export type TimeConfig =
-    '10sec' | '20sec' | '30sec' |
-    '1min' | '3min' | '5min' | '10min' | '15min' | '20min' | '30min' | '45min' |
+    '5sec' | '10sec' | '15sec' | '20sec' | '25sec' | '30sec' | '35sec' | '40sec' | '45sec' | '50sec' | '55sec' |
+    '1min' | '2min' | '3min' | '4min' | '5min' | '10min' | '15min' | '20min' | '25min' | '30min' | '35min' | '40min' | '45min' | '50min' | '55min' |
     '1hour' | '2hour' | '3hour' | '4hour' | '5hour' | '6hour' | '7hour' | '8hour' |
     '9hour' | '10hour' | '11hour' | '12hour' | '1day'
 
@@ -17,28 +17,58 @@ export function dateFormat(date: Date | string, formate: DateFormat = 'DD/MM/YYY
 
 export function timeCofig(timeout: TimeConfig) {
     switch (timeout) {
+        case '5sec':
+            return 5 * 1000
         case '10sec':
             return 10 * 1000
+        case '15sec':
+            return 15 * 1000
         case '20sec':
             return 20 * 1000
+        case '25sec':
+            return 25 * 1000
         case '30sec':
             return 30 * 1000
+        case '35sec':
+            return 35 * 1000
+        case '40sec':
+            return 40 * 1000
+        case '45sec':
+            return 45 * 1000
+        case '50sec':
+            return 50 * 1000
+        case '55sec':
+            return 55 * 1000
         case '1min':
             return 60 * 1000
+        case '2min':
+            return 2 * 60 * 1000
         case '3min':
             return 3 * 60 * 1000
+        case '4min':
+            return 4 * 60 * 1000
         case '5min':
             return 5 * 60 * 1000
         case '10min':
             return 10 * 60 * 1000
-        case '20min':
-            return 20 * 60 * 1000
         case '15min':
             return 15 * 60 * 1000
+        case '20min':
+            return 20 * 60 * 1000
+        case '25min':
+            return 25 * 60 * 1000
         case '30min':
             return 30 * 60 * 1000
+        case '35min':
+            return 35 * 60 * 1000
+        case '40min':
+            return 40 * 60 * 1000
         case '45min':
             return 45 * 60 * 1000
+        case '50min':
+            return 50 * 60 * 1000
+        case '55min':
+            return 55 * 60 * 1000
         case '1hour':
             return 60 * 60 * 1000
         case '2hour':
