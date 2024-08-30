3e460529441093e48667be7a2fa3350ae4096a7f
Jutipong <techcom.ec20@gmail.com>
2024-08-30 19:40:38 +0700

    refactor: Update select2 component to use debounced search and fetch data asynchronously

--- src/components/@v/select2.vue
+++ src/components/@v/select2.vue
@@ -1,46 +1,46 @@
 <script setup lang="ts">
-import { fa } from 'vuetify/locale'
+import { watchDebounced } from '@vueuse/core'
 
-const search = ref('')
+const textSearch = ref('')
+const dataSelected = ref<Item | null>(null)
 const items = ref([] as Item[])
 const isLoading = ref(false)
-const selected = ref<Item | null>(null)
-const isLoadData = ref(false)
+const isFetchData = ref(false)
+const minimumCharacters = ref(2)
+
+const placeholder = computed(() => `minimum ${minimumCharacters.value} characters`)
+const noDataText = computed(() => isLoading.value ? 'Loading...' : 'No data found')
 
 async function fetchData(val?: string) {
-    return await api.Post<any>('customer/MasterSelect2', { name: val, pageSize: 10 }, {
-        baseURL: 'http://localhost:5224/',
-        isLoading: false,
-    })
-}
-
-const onInput = _.debounce(async (val: string) => {
-    if (!isLoadData.value)
+    if (!isFetchData.value)
         return
 
     isLoading.value = true
-    items.value = await fetchData(val)
-    isLoading.value = false
-}, 600)
 
-onMounted(() => {
-    watch(search, (val: string) => {
-        if (val.length < 2)
-            return
-
-        onInput(val)
+    items.value = await api.Post<any>('customer/MasterSelect2', { name: val, pageSize: 10 }, {
+        baseURL: 'http://localhost:5224/',
+        isLoading: false,
     })
-})
+
+    isLoading.value = false
+}
+
+watchDebounced(textSearch, async (val: string) => {
+    if (val.length < 2)
+        return
+
+    await fetchData(val)
+}, { debounce: 1000 })
 
 function onMenu(val: boolean) {
-    isLoadData.value = val
+    isFetchData.value = val
 }
 </script>
 
 <template>
     <v-autocomplete
-        v-model:search="search"
-        v-model="selected"
+        v-model:search="textSearch"
+        v-model="dataSelected"
         :dirty="true"
         label="Select2"
         :items="items"
@@ -50,7 +50,8 @@ function onMenu(val: boolean) {
         item-value="id"
         clearable
         return-object
-        placeholder="minimum 2 characters"
+        :placeholder="placeholder"
+        :no-data-text="noDataText"
         @update:menu="onMenu"
     />
 </template>
