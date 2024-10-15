7494115e8643d21e357eca8593fa48f9017a788c
Js.dev@win11 <techcom.ec20@gmail.com>
2024-10-15 16:15:04 +0700

    Refactor auto-imports to add storeToRefs from pinia

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -45,6 +45,7 @@ declare global {
   const requiredNumber: typeof import('./utils/zod-validate')['requiredNumber']
   const requiredString: typeof import('./utils/zod-validate')['requiredString']
   const shallowRef: typeof import('vue')['shallowRef']
+  const storeToRefs: typeof import('pinia')['storeToRefs']
   const toRef: typeof import('vue')['toRef']
   const toRefs: typeof import('vue')['toRefs']
   const useAppStore: typeof import('./stores/app')['useAppStore']
