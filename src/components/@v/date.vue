4fa699550644e6d7e4b005c3e1a47fa95759f571
T660046 <T660046@TCRBANK.COM>
2024-12-12 14:38:03 +0700

    Refactor date component to use props and emits for modelValue; implement state management and clear functionality

--- src/components/@v/date.vue
+++ src/components/@v/date.vue
@@ -1,12 +1,42 @@
 <script lang="ts" setup>
-const value = defineModel<Date | null>()
+const props = defineProps<{ modelValue: Date | null | undefined }>()
+const emit = defineEmits<{ 'update:modelValue': [value: Date | null] }>()
+
+const state = ref<Date | null>()
+
+function onClear() {
+    state.value = null
+    emit('update:modelValue', null)
+}
+
+onMounted(() => {
+    watch(() => props.modelValue, (newVal, oldVal) => {
+        if (`${newVal}` === `${oldVal}`)
+            return
+
+        if (`${state.value}` === `${newVal}`)
+            return
+
+        state.value = newVal ? new Date(newVal) : null
+    })
+
+    watch(state, (newVal, oldVal) => {
+        if (`${newVal}` === `${oldVal}`)
+            return
+
+        if (`${newVal}` === `${props.modelValue}`)
+            return
+
+        emit('update:modelValue', newVal ?? null)
+    })
+})
 </script>
 
 <template>
     <VDateInput
-        v-model="value"
+        v-model="state"
         autocomplete="off"
         hide-details="auto"
-        @click:clear="value = null"
+        @click:clear="onClear()"
     />
 </template>
