2624576aabb144283f2c58afb30dbf0fe7c18ded
Jutipong.Dev <techcom.ec20@gmail.com>
2024-12-17 00:10:49 +0700

    Refactor modal component template to remove unnecessary Teleport wrapper for improved structure

--- src/components/product/modal.vue
+++ src/components/product/modal.vue
@@ -53,53 +53,49 @@ defineExpose({
 </script>
 
 <template>
-    <Teleport to="body">
-        <VRow justify="center">
-            <VDialog v-model="state.open" persistent width="1024">
-                <VCard>
-                    <VCardTitle>
-                        <VChip color="success" :prepend-icon="titleIcon">
-                            {{ title }} Product
-                        </VChip>
-                    </VCardTitle>
+    <VDialog v-model="state.open" persistent width="1024">
+        <VCard>
+            <VCardTitle>
+                <VChip color="success" :prepend-icon="titleIcon">
+                    {{ title }} Product
+                </VChip>
+            </VCardTitle>
 
-                    <VCardText>
-                        <VRow>
-                            <VCol cols="12" md="4">
-                                <VTextField v-model="state.product.title" label="Title" />
-                            </VCol>
-                            <VCol cols="12" md="4">
-                                <VCurrency v-model="state.product.price" label="Price" />
-                            </VCol>
-                            <VCol cols="12" md="4">
-                                <VTextField v-model="state.product.rating" label="Rating" />
-                            </VCol>
-                        </VRow>
-                        <VRow>
-                            <VCol cols="12" md="4">
-                                <VTextField v-model="state.product.stock" label="Stock" />
-                            </VCol>
-                            <VCol cols="12" md="4">
-                                <VTextField v-model="state.product.brand" label="Brand" />
-                            </VCol>
-                            <VCol cols="12" md="4">
-                                <VNumberInput
-                                    v-model="state.product.discountPercentage"
-                                    density="compact"
-                                    color="primary"
-                                    variant="outlined"
-                                    label="Discount"
-                                />
-                            </VCol>
-                        </VRow>
-                    </VCardText>
+            <VCardText>
+                <VRow>
+                    <VCol cols="12" md="4">
+                        <VTextField v-model="state.product.title" label="Title" />
+                    </VCol>
+                    <VCol cols="12" md="4">
+                        <VCurrency v-model="state.product.price" label="Price" />
+                    </VCol>
+                    <VCol cols="12" md="4">
+                        <VTextField v-model="state.product.rating" label="Rating" />
+                    </VCol>
+                </VRow>
+                <VRow>
+                    <VCol cols="12" md="4">
+                        <VTextField v-model="state.product.stock" label="Stock" />
+                    </VCol>
+                    <VCol cols="12" md="4">
+                        <VTextField v-model="state.product.brand" label="Brand" />
+                    </VCol>
+                    <VCol cols="12" md="4">
+                        <VNumberInput
+                            v-model="state.product.discountPercentage"
+                            density="compact"
+                            color="primary"
+                            variant="outlined"
+                            label="Discount"
+                        />
+                    </VCol>
+                </VRow>
+            </VCardText>
 
-                    <VCardActions>
-                        <VBtn color="warning" prepend-icon="mdi-close" text="Close" @click="func.onClose()" />
-                        <VBtn color="primary" prepend-icon="mdi-content-save" text="Save" :loading="isLoading" @click="func.onAction()" />
-                    </VCardActions>
-                </VCard>
-            </VDialog>
-        </VRow>
-    </Teleport>
+            <VCardActions>
+                <VBtn color="warning" prepend-icon="mdi-close" text="Close" @click="func.onClose()" />
+                <VBtn color="primary" prepend-icon="mdi-content-save" text="Save" :loading="isLoading" @click="func.onAction()" />
+            </VCardActions>
+        </VCard>
+    </VDialog>
 </template>
